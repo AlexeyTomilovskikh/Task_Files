71b11daf30fc696209887371e464e64bae8eb158
Alexey Tomilovskikh <alexey.tomilovskikh@gmail.com>
2021-03-19 23:53:40 +0500

    Task 1.2

--- Cook_book_Task 1,2.py
+++ Cook_book_Task 1,2.py
@@ -1,81 +1,42 @@
-print("Книга рецептов")
-with open("recipes.txt") as recipe:
-    print(recipe.read())
-
-    recipe.seek(0)
-
-    def output_cook_book(recipes):  
-        cook_book = {}
-    
-        while True:
-            dish = recipes.readline().strip()        
-            if not dish:
-                break
-
-            number = int(recipes.readline().strip())        
-        
+def create_cook_book(file_name):
+    cook_book = {}
+    with open('recipes.txt', encoding='utf-8') as f:
+        for line in f:
+            dish_name = line.lower().strip().capitalize()
+            counter = int(f.readline())
             ingredients = []
-            cook_book[dish] = ingredients
+            for i in range(counter):
+                names = ['ingredient_name', 'quantity', 'measure']
+                temp_dict = dict(zip(names, (f.readline().strip('\n').split(' | '))))
+                temp_dict['quantity'] = int(temp_dict['quantity'])
+                ingredients.append(temp_dict)
+            cook_book[dish_name] = ingredients
+            f.readline()
+    return cook_book
 
-            count = 0
-            while count < number:
-                ingredient = recipes.readline().strip()
-                ingredient_list = ingredient.split("|")            
-                ingredients.append({"ingredient_name": ingredient_list[0], "quantity": ingredient_list[1], "measure": ingredient_list[2]})
-                count +=1
+def get_shop_list_by_dishes(dishes, person):
+    cook_dict = create_cook_book('recipes.txt')
+    name_list = []
+    ingredien_list = []
+    prop_dict = {}
+    for dish in dishes:
+      if dish in cook_dict:
+        dict_list = cook_dict[dish]
+        for elem_dict in dict_list:
+          if elem_dict['ingredient_name'] in prop_dict:
+            x = elem_dict['ingredient_name']
+            y = elem_dict['quantity'] * person
+            prop_dict[x]['quantity'] += y
+          else:
+            elem_dict['quantity'] *= person
+            ingredien_list.append(elem_dict)
+            name_list.append(elem_dict.pop('ingredient_name'))
+      prop_dict.update(dict(zip(name_list, ingredien_list)))
+    return prop_dict
 
-            recipes.readline().strip()     
-        
-        return cook_book
-
-    print() 
-    cook_book = output_cook_book(recipe)      
-    for dish in cook_book:
-        print(dish)
-        for ingredient in cook_book[dish]:
-            print(ingredient)
-
-    recipe.seek(0)
-
-    def get_shop_list_by_dishes(dishes, person_count):
-        shop_dict = {}
-
-        while True:
-            dish = recipe.readline().strip()        
-            if not dish:
-                break
-
-            number = int(recipe.readline().strip())
-            
-            for ingredient in range(number):
-                ingredient = recipe.readline().strip()
-                ingredient_list = ingredient.split("|")                
-                repetition = dishes.count(dish)
-                quantity = int(ingredient_list[1]) * person * repetition                
-                if dish in dishes:
-                    if ingredient_list[0] not in shop_dict:
-                        shop_dict[ingredient_list[0]] = {'measure': ingredient_list[2], 'quantity': quantity}
-                    else:                        
-                        quantity += int(shop_dict[ingredient_list[0]]['quantity'])
-                        shop_dict[ingredient_list[0]] = {'measure': ingredient_list[2], 'quantity': quantity}            
-                                       
-            recipe.readline().strip()
-
-        return shop_dict
-
-    try:
-        food = input('Перечислите блюда через запятую, которые нужно приготовить: ')
-        person = int(input('Укажите количество порций: '))
-        print('Состав блюд: ')
-    except Exception as e:
-        print(e)
-        print('Вы ошиблись при вводе')
-        print('Состав блюд для Омлета,Утки по-пекински,Запеченного картофеля,Фахитос на одну порцию')
-        food = 'Омлет,Утка по-пекински,Запеченый картофель,Фахитос'
-        person = 1    
-    
-    dishes_list = food.split(',')           
-        
-    shop_dict = get_shop_list_by_dishes(dishes_list, person)
-    for ingredient in shop_dict:
-        print(ingredient, shop_dict[ingredient])
+with open("recipes.txt") as recipe:
+  print("Книга рецептов")
+  print(recipe.read())
+food = input('Какое блюдо из списка приготовить?: ')
+p = int(input('Укажите количество порций: '))
+print(get_shop_list_by_dishes([food], p))
